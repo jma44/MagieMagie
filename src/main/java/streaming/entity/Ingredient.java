19ee0b6d7610ac3b3867992f642defac81a9fba4
Quentin12000 <lestesterindu51@hotmail.fr>
2016-06-06 11:57:59 +0200

    Ajout Entites

---
+++ src/main/java/streaming/entity/Ingredient.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author ajc
+ */
+@Entity
+public class Ingredient implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    
+    public enum Type {CORNE_DE_LICORNE, BAVE_DE_CRAPAUD, SANG_DE_VIERGE, LAPIS_LAZULI, AILE_DE_CHAUVE_SOURIS}
+    
+    @Enumerated(EnumType.STRING)
+    private Type typeIngredient;
+    
+    @ManyToOne
+    @JoinTable(name = "joueur_id")
+    private Joueur joueur;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ingredient)) {
+            return false;
+        }
+        Ingredient other = (Ingredient) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "streaming.entity.Ingredient[ id=" + id + " ]";
+    }
+
+    public Type getTypeIngredient() {
+        return typeIngredient;
+    }
+
+    public void setTypeIngredient(Type typeIngredient) {
+        this.typeIngredient = typeIngredient;
+    }
+
+    public Joueur getJoueur() {
+        return joueur;
+    }
+
+    public void setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+}
---
+++ src/main/java/streaming/entity/Joueur.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author ajc
+ */
+@Entity
+public class Joueur implements Serializable 
+{
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String pseudo;
+    
+    private int nbreCartes;
+    
+    private Date dateConnexion;//Changer le format poyr obtenir les secondes et minutes
+    
+    @OneToMany(mappedBy = "joueur")
+    private List<Ingredient> ingredients;
+    
+    private boolean actif;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Joueur)) {
+            return false;
+        }
+        Joueur other = (Joueur) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "streaming.entity.Joueur[ id=" + id + " ]";
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public int getNbreCartes() {
+        return nbreCartes;
+    }
+
+    public void setNbreCartes(int nbreCartes) {
+        this.nbreCartes = nbreCartes;
+    }
+
+    public Date getDateConnexion() {
+        return dateConnexion;
+    }
+
+    public void setDateConnexion(Date dateConnexion) {
+        this.dateConnexion = dateConnexion;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
+    
+}
